b6ed50f48ae7904725a02076d7ccb67c75133927
Aakankshap16 <aakankshap971@gmail.com>
2023-03-25 12:25:59 +0530

    Refactor all 3 to one generic method

--- Program.cs
+++ Program.cs
@@ -7,15 +7,20 @@ namespace GenreicDemo
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to Generic Program");
             Console.WriteLine("Please Enter the FirstValue:");
-            string str1 = Console.ReadLine();
+            var val1 = Console.ReadLine();
             Console.WriteLine("Please Enter the SecondValue:");
-            string str2 = Console.ReadLine();
+            var val2 = Console.ReadLine();
             Console.WriteLine("Please Enter the ThirdValue:");
-            string str3 = Console.ReadLine();
-            string val = FindMax.MaximumVal(str1, str2, str3);
-            Console.WriteLine("{0} is greater", val);
+            var val3 = Console.ReadLine();
+
+            var Value =  FindMax.MaximumNum(val1,val2,val3);
+           Console.WriteLine("{0} is greater", Value);
+            Console.WriteLine("**************************************");
+
+
+
+
         }
 
     }
